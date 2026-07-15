43f3dd32b24725c15acfeca629f3d11e6804dae8
James Orson <jamesaorson@gmail.com>
2026-07-15 14:14:19 -0700

    Fix compile errors
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- include/summa/hashmap/hashmap.h
+++ include/summa/hashmap/hashmap.h
@@ -15,9 +15,13 @@ SummaHashCode summa_hash(void* value, size_t size);
 
 #pragma region Hash maps
 
-typedef struct SummaHashMap     SummaHashMap;
-typedef struct SummaHashIndices SummaHashIndices;
-typedef struct SummaHashValues  SummaHashValues;
+typedef struct summa_hash_map_t* SummaHashMap;
+
+#define SUMMA_ARRAY_IMPLEMENTATION
+#include <summa/array/array.h>
+
+SUMMA_ARRAY_GENERATE_TYPE_DEF(SummaHashIndices, hash_indices, SummaHashCode)
+SUMMA_ARRAY_GENERATE_TYPE_DEF(SummaHashValues, hash_values, void*)
 
 SummaHashMap summa_hash_map_make(void** values, size_t num_elements, size_t element_size);
 SummaHashMap summa_hash_map_make_empty(size_t element_size);
@@ -33,6 +37,9 @@ void         summa_hash_map_free(SummaHashMap map);
 #ifndef SUMMA_HASHMAP_IMPLEMENTATION_ONCE
 #define SUMMA_HASHMAP_IMPLEMENTATION_ONCE
 
+#include <stdlib.h>
+#include <string.h>
+
 #pragma region Hash codes
 
 // Reference: https://www.cse.yorku.ca/~oz/hash.html
@@ -44,7 +51,7 @@ void         summa_hash_map_free(SummaHashMap map);
 SummaHashCode summa_hash(void* value, size_t size) {
     SummaHashCodeMutable hash = _SUMMA_HASH_MAGIC;
 
-    for (int i = 0; i < size; i++) {
+    for (size_t i = 0; i < size; i++) {
         int c = ((char*)value)[i];
         _SUMMA_HASH_IMPL(hash, c);
     }
@@ -56,34 +63,30 @@ SummaHashCode summa_hash(void* value, size_t size) {
 
 #pragma region Hash maps
 
-#define SUMMA_ARRAY_IMPLEMENTATION
-#include <summa/array/array.h>
-
-SUMMA_ARRAY_GENERATE_TYPE(SummaHashIndices, hash_indices, SummaHashCode)
-SUMMA_ARRAY_GENERATE_TYPE(SummaHashValues, hash_values, void*)
+SUMMA_ARRAY_GENERATE_TYPE_IMPL(SummaHashIndices, hash_indices, SummaHashCode)
+SUMMA_ARRAY_GENERATE_TYPE_IMPL(SummaHashValues, hash_values, void*)
 
 struct SummaHashMap {
     SummaHashIndices indices;
     SummaHashValues  values;
 };
 
-typedef struct {
+struct summa_hash_map_t {
     SummaHashIndices indices;
     SummaHashValues  values;
     size_t           element_size;
-} summa_hash_map_t;
-typedef summa_hash_map_t* SummaHashMap;
+};
 
 SummaHashMap summa_hash_map_make(void** values, size_t num_elements, size_t element_size) {
-    SummaHashMap map  = (SummaHashMap)malloc(sizeof(summa_hash_map_t));
+    SummaHashMap map  = (SummaHashMap)malloc(sizeof(struct summa_hash_map_t));
     map->element_size = element_size;
 
     map->indices = summa_hash_indices_make_empty();
     map->values  = summa_hash_values_make_empty();
 
-    for (int i = 0; i < num_elements; i++) {
+    for (size_t i = 0; i < num_elements; i++) {
         SummaHashCode code = summa_hash(values[i], num_elements * element_size);
-        if (!summa_hash_indices_contains(map->indices, code)) {
+        if (!summa_hash_indices_contains(map->indices, &code)) {
             // TODO: Add to next empty index, adding a new slot to the end if needed
             // TODO: Add value to same i as new slot
         }
@@ -93,7 +96,7 @@ SummaHashMap summa_hash_map_make(void** values, size_t num_elements, size_t elem
 }
 
 SummaHashMap summa_hash_map_make_empty(size_t element_size) {
-    SummaHashMap map  = (SummaHashMap)malloc(sizeof(summa_hash_map_t));
+    SummaHashMap map  = (SummaHashMap)malloc(sizeof(struct summa_hash_map_t));
     map->element_size = element_size;
     map->indices      = summa_hash_indices_make_empty();
     map->values       = summa_hash_values_make_empty();
@@ -105,15 +108,14 @@ void summa_hash_map_clear(SummaHashMap map) {
 }
 
 void summa_hash_map_copy(SummaHashMap dest, SummaHashMap src) {
-    SummaHashMap map   = (SummaHashMap)malloc(sizeof(summa_hash_map_t));
     dest->element_size = src->element_size;
     if (dest->indices) {
         summa_hash_indices_free(dest->indices);
-        summa_hash_indices_make_empty(dest->indices);
+        dest->indices = summa_hash_indices_make_empty();
     }
     if (dest->values) {
         summa_hash_values_free(dest->values);
-        summa_hash_values_make_empty(dest->values);
+        dest->values = summa_hash_values_make_empty();
     }
     summa_hash_indices_copy(dest->indices, src->indices);
     summa_hash_values_copy(dest->values, src->values);
